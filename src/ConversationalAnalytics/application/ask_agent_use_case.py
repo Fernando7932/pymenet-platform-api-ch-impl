4e21d91c43dac06a932448fec8202a45874e6355
kowisprox <nicolaskouacosta2005@gmail.com>
2026-07-13 23:27:02 -0500

    feat(conversational/application): implement AskAgentUseCase

---
+++ src/ConversationalAnalytics/application/ask_agent_use_case.py
@@ -0,0 +1,15 @@
+from src.ConversationalAnalytics.domain.value_objects import AnalyticContext, AgentInterface
+
+class AskAgentUseCase:
+    # Inyección de dependencias
+
+    def _init_(self, agent_client: AgentInterface):
+        self.agent_client = agent_client
+
+    def execute(self, user_query: str, file_uri: str, file_description: str) -> dict:
+        # Validaciones + domain logic
+        analytic_context = AnalyticContext(user_query, file_uri, file_description)
+
+        # Interface call
+        agent_response = self.agent_client.ask(analytic_context)
+        return agent_response
